600abdae347390155badd4ee59a85fa3762d7f89
ncutixavier <ncuti60@gmail.com>
2022-04-23 12:37:55 +0200

    add listing array

--- js/index.js
+++ js/index.js
@@ -1,28 +1,57 @@
-// !! IMPORTANT README:
-
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
 "use strict";
+
+const history = [
+  {
+    id: 1,
+    year: "1914",
+    details: "Born in Cresco, Iowa",
+  },
+  {
+    id: 2,
+    year: "1933",
+    details:
+      'Leaves his family\'s farm to attend the University of Minnesota, thanks to a Depression era program known as the "National Youth Administration"',
+  },
+  {
+    id: 3,
+    year: "1936",
+    details:
+      "Graduates from the University of Minnesota with a degree in economics",
+  },
+  {
+    id: 4,
+    year: "1939",
+    details:
+      "Starts working for the University of Minnesota's Office of Public Affairs",
+  },
+  {
+    id: 5,
+    year: "1940",
+    details: "Joins the National Labor Relations Board",
+  },
+  {
+    id: 6,
+    year: "1941",
+    details: "Retires from the University of Minnesota",
+  },
+  {
+    id: 7,
+    year: "1942",
+    details: "Starts working for the National Labor Relations Board",
+  },
+  {
+    id: 8,
+    year: "1943",
+    details: "Retires from the National Labor Relations Board",
+  },
+  {
+    id: 9,
+    year: "1944",
+    details: "Starts working for the National Labor Relations Board",
+  },
+  {
+    id: 10,
+    year: "1945",
+    details: "Retires from the National Labor Relations Board",
+  },
+];
